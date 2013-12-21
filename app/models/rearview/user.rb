8649933dbb1f0b656324d10e6bc577680b354afe
Trent Albright <trent.albright@gmail.com>
2013-12-21 08:48:32 -0500

    Setup devise for database auth only by default

--- app/models/rearview/user.rb
+++ app/models/rearview/user.rb
@@ -1,7 +1,4 @@
 class Rearview::User < ActiveRecord::Base
   include Rearview::Concerns::Models::User
-  devise :omniauthable,:database_authenticatable,:omniauth_providers => [:google_oauth2]
-  def self.valid_email?(email)
-    email.present? && (email.ends_with?('hungrymachine.com')|| email.ends_with?('livingsocial.com'))
-  end
+  devise :database_authenticatable
 end
