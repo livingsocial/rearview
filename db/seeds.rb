e5f298579a5f2b7412fd800dbd861c3701ac8cff
Trent Albright <trent.albright@gmail.com>
2013-12-21 21:36:36 -0500

    Add seed for creating default user

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+admin = Rearview::User.new do |u|
+  u.email = "admin@localhost"
+  u.password = "admin"
+end
+admin.save!
