4bd74b86c1b5b08a2f31cd5f5fce62d346141b55
Trent Albright <trent.albright@gmail.com>
2014-01-23 23:13:06 -0500

    Add some logging for the cursed crytography issue with rails and jruby

--- config/initializers/unlimited_strength_cryptography.rb
+++ config/initializers/unlimited_strength_cryptography.rb
@@ -2,4 +2,15 @@
 begin
   java.lang.Class.for_name('javax.crypto.JceSecurity').get_declared_field('isRestricted').tap{|f| f.accessible = true; f.set nil, false}
 rescue
+  Rails.logger.warn <<-EOF
+
+    You may not have unlimited strength cryptography installed. There's a
+    known issue with Rails 4.0, jruby, and cryptography in the JVM. Please visit
+    https://github.com/jruby/jruby/wiki/UnlimitedStrengthCrypto if you encounter
+    failures during startup. If things are running fine you can ignore this message
+    and remove the intializer.
+
+    Exception: #{$!}
+    Trace: #{$@.join("\n")}
+  EOF
 end
